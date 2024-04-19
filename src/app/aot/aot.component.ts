f7c4cf5eacb13762f0a2eb82c11fd12cdc833f86
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-04-19 15:36:54 +0200

    aot works!

---
+++ src/app/aot/aot.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aot',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './aot.component.html',
+  styleUrl: './aot.component.scss',
+})
+export class AotComponent {}
