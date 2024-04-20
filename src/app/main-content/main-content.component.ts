566e5a980dcf5ca6e979bd8c25a0f37c13cc5994
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-04-20 16:41:58 +0200

    Skill set
    started

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LandingPageComponent } from '../landing-page/landing-page.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
+import { SkillSetComponent } from '../skill-set/skill-set.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent, AboutMeComponent],
+  imports: [
+    CommonModule,
+    LandingPageComponent,
+    AboutMeComponent,
+    SkillSetComponent,
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
