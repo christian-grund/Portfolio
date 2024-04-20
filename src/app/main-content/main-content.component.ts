2cc6d0895629b596d363327ca139991392ca1c56
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-04-20 16:07:54 +0200

    About Me Section
    works!

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LandingPageComponent } from '../landing-page/landing-page.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent],
+  imports: [CommonModule, LandingPageComponent, AboutMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
