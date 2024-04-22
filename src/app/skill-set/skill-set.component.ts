e09af9b78ff0eac12029842421fcee4de87e5b42
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-04-22 06:03:45 +0200

    Skill set
    finished

--- src/app/skill-set/skill-set.component.ts
+++ src/app/skill-set/skill-set.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skill-set',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skill-set.component.html',
-  styleUrl: './skill-set.component.scss'
+  styleUrl: './skill-set.component.scss',
 })
 export class SkillSetComponent {
+  skillSetIconsTop = [
+    'angular.svg',
+    'typescript.svg',
+    'javascript.svg',
+    'html.svg',
+    'scrum.svg',
+  ];
+  skillSetIconsBottom = [
+    'firebase.svg',
+    'git.svg',
+    'css.svg',
+    'api.svg',
+    'material_design.svg',
+  ];
 
+  skillSetNamesTop = ['Angular', 'TypeScript', 'JavaScript', 'HTML', 'Scrum'];
+  skillSetNamesBottom = [
+    'Firebase',
+    'GIT',
+    'CSS',
+    'Rest-Api',
+    'Material Design',
+  ];
 }
