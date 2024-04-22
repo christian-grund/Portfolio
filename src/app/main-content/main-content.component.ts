7598204b7a1cfe24f1dea6831f94e103f0cd8b3d
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-04-22 13:34:17 +0200

    Moved Content to maincontent component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { LandingPageComponent } from '../landing-page/landing-page.component';
-import { AboutMeComponent } from '../about-me/about-me.component';
-import { SkillSetComponent } from '../skill-set/skill-set.component';
-import { MyWorkComponent } from '../my-work/my-work.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
+import { MyWorkComponent } from './my-work/my-work.component';
 
 @Component({
   selector: 'app-main-content',
