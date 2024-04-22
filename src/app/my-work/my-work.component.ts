e57d22f05002091000a96b45513af55e525f0bac
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-04-22 12:35:45 +0200

    My Projects
    - mvp finished

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -8,4 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './my-work.component.html',
   styleUrl: './my-work.component.scss',
 })
-export class MyWorkComponent {}
+export class MyWorkComponent {
+  projectImages = [
+    'join.png',
+
+    'pollo_loco.png',
+    'simple_crm.png',
+    'pokedex.png',
+  ];
+  projectNames = ['Join', 'Pollo Loco', 'Simple CRM', 'Pokédex'];
+  projectLanguages = [
+    'Angular | TypeScript | HTML | CSS | Firebase',
+    'JavaScript | HTML | CSS',
+    'Angular | Firebase',
+    'JavaScript | HTML | CSS | Api',
+  ];
+  projectDescriptions = [
+    'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+    'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+    'A very Simple Customer Relationship Management system working with CRUD functionality. ',
+    'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
+  ];
+  projectLinks = [];
+}
