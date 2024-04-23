83c94e13abe61100588eef3cf613a618eccb045c
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-04-23 11:15:36 +0200

    Contact
    - started
    - finished footer mvp

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,6 +4,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillSetComponent } from './skill-set/skill-set.component';
 import { MyWorkComponent } from './my-work/my-work.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
@@ -14,6 +15,7 @@ import { MyWorkComponent } from './my-work/my-work.component';
     AboutMeComponent,
     SkillSetComponent,
     MyWorkComponent,
+    ContactComponent,
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
