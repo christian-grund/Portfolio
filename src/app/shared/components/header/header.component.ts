3d985a1659d7a77360fa46a53b35b94331e0127f
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-05-13 09:09:12 +0200

    Responsive design

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,23 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TranslateModule, HttpClientModule, RouterLink],
+  imports: [TranslateModule, HttpClientModule, RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   underlineActive: number | null = null;
   isOverlayActive: boolean = false;
+  selectedLanguage: string = 'en';
 
   constructor(public translateService: TranslateService) {}
 
   changeLanguage(langCode: string) {
     this.translateService.use(langCode);
+    this.selectedLanguage = langCode;
+    console.log(this.selectedLanguage);
   }
 
   toggleUnderline(index: number): void {
